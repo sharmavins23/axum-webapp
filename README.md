3554bcbb9da767e4c5a9994cbdfea4be18d4ac5e
sharmavins23 <sharmavins23@gmail.com>
2026-02-15 16:14:16 -0800

    Initialize Axum web application with basic structure and dependencies

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rust: Axum WebApp
+
+This project is a follow-along of
+[Jeremy Chone's Rust Production Coding: Web App](https://www.youtube.com/playlist?list=PL7r-PXl6ZPcCTTxjmsb9bFZB9i01fAtI7)
+series. It implements a production-style web application with a variety of
+different features.
+
+## Implemented Features
+
+- Simple HTTP `GET` routing and responses.
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![deny(clippy::expect_used)]
+#![deny(clippy::unwrap_used)]
+#![forbid(unsafe_code)]
+#![allow(unused)] // Only for starting out!
+
+// ===== Imports ===============================================================
+
+use std::net::SocketAddr;
+
+use axum::{Router, Server, response::Html, routing::get};
+
+// ===== Driver code ===========================================================
+
+/// Main entry point for the application.
+#[tokio::main]
+async fn main() {
+    let routes_hello =
+        Router::new().route("/hello", get(|| async { Html("<h1>Hello, world!</h1>") }));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on http://{address}");
+
+    Server::bind(&address)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
